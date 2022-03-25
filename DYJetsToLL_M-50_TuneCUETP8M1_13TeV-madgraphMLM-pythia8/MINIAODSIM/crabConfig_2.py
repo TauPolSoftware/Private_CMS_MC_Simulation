67ad11f8c3c537337ad5aaee65f0408cf7430d3a
Clement <clement.grimault@CERN.CH>
2022-03-25 18:09:18 +0100

    Update config files for miniAOD legacy reprocessing

--- DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/crabConfig_2.py
+++ DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/crabConfig_2.py
@@ -3,7 +3,7 @@ from CRABClient.UserUtilities import config
 config = config()
 
 config.section_("General")
-config.General.requestName = "my_miniaod_DYJetsToTauTau_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_ext1"
+config.General.requestName = "my_miniaod_DYJetsToTauTau_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_ext2"
 config.General.workArea = "/opt/sbg/cms/ui2_data1/cgrimault/private_mc_workdir/"
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -12,7 +12,7 @@ config.section_("JobType")
 config.JobType.pluginName = "Analysis"
 config.JobType.psetName = "SMP-RunIISummer16MiniAODv3-00001_1_cfg.py"
 #config.JobType.inputFiles = ["test.sh", "private_DYJetsToTauTau_inputFiles.txt"]
-config.JobType.inputFiles = ["private_dy_input_files_ext1.txt"]
+config.JobType.inputFiles = ["private_dy_input_files_ext2.txt"]
 #config.JobType.scriptExe = "./test.sh"
 
 config.section_("Data")
@@ -24,7 +24,7 @@ config.section_("Data")
 #config.Data.publishDBS = "phys03"
 
 #### TO ADD !!!!! ####
-config.Data.userInputFiles = open('private_dy_input_files_ext1.txt').readlines()
+config.Data.userInputFiles = open('private_dy_input_files_ext2.txt').readlines()
 
 #config.Data.userInputFiles = ["root://sbgse1.in2p3.fr://dpm/in2p3.fr/home/cms/phedex/store/user/cgrimaul/tau_polarisation/aachen/private_mc/DYJetsToTauTau_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16DR80Premix-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/191007_095808/0000/SUS-RunIISummer16DR80Premix-00036_step1_100.root"]
 
@@ -33,7 +33,7 @@ config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
 config.Data.outLFNDirBase = "/store/user/cgrimaul/private_mc"
 config.Data.outputPrimaryDataset = "DYJetsToTauTau_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"
-config.Data.outputDatasetTag = "RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext1"
+config.Data.outputDatasetTag = "RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext2"
 
 config.section_("Site")
 config.Site.storageSite = "T2_FR_IPHC"
--- DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/test.sh
+++ DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/test.sh
@@ -1,40 +1,43 @@
 #!/bin/bash
-export SCRAM_ARCH=slc6_amd64_gcc530
+export SCRAM_ARCH=slc6_amd64_gcc630
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-if [ -r CMSSW_8_0_21/src ] ; then 
- echo release CMSSW_8_0_21 already exists
+if [ -r CMSSW_9_4_9/src ] ; then 
+ echo release CMSSW_9_4_9 already exists
 else
-scram p CMSSW CMSSW_8_0_21
+scram p CMSSW CMSSW_9_4_9
 fi
-cd CMSSW_8_0_21/src
+cd CMSSW_9_4_9/src
 eval `scram runtime -sh`
 
 
 scram b
 cd ../../
 #cmsDriver.py step1 --filein "dbs:/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16DR80Premix-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/AODSIM" --fileout file:SUS-RunIISummer16MiniAODv2-00001.root --mc --eventcontent MINIAODSIM --runUnscheduled --datatier MINIAODSIM --conditions 80X_mcRun2_asymptotic_2016_TrancheIV_v6 --step PAT --nThreads 1 --era Run2_2016 --python_filename SUS-RunIISummer16MiniAODv2-00001_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 10 || exit $? ; 
-cmsDriver.py step1 --filein "file:/home/home2/institut_3b/tmuller/home/cms/htt/generator/DYJetsToLL_M-50_reproduction/AODSIM/SUS-RunIISummer16DR80Premix-00036.root" --fileout file:SUS-RunIISummer16MiniAODv2-00001.root --mc --eventcontent MINIAODSIM --runUnscheduled --datatier MINIAODSIM --conditions 80X_mcRun2_asymptotic_2016_TrancheIV_v6 --step PAT --nThreads 1 --era Run2_2016 --python_filename SUS-RunIISummer16MiniAODv2-00001_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 10 || exit $? ; 
-#cmsRun -e -j SUS-RunIISummer16MiniAODv2-00001_rt.xml SUS-RunIISummer16MiniAODv2-00001_1_cfg.py || exit $? ; 
-cmsRun -e -j SUS-RunIISummer16MiniAODv2-00001_rt.xml -p PSet.py || exit $? ; 
-cp SUS-RunIISummer16MiniAODv2-00001_rt.xml FrameworkJobReport.xml
-echo 10 events were ran 
-grep "TotalEvents" SUS-RunIISummer16MiniAODv2-00001_rt.xml 
-if [ $? -eq 0 ]; then
-    grep "Timing-tstoragefile-write-totalMegabytes" SUS-RunIISummer16MiniAODv2-00001_rt.xml 
-    if [ $? -eq 0 ]; then
-        events=$(grep "TotalEvents" SUS-RunIISummer16MiniAODv2-00001_rt.xml | tail -1 | sed "s/.*>\(.*\)<.*/\1/")
-        size=$(grep "Timing-tstoragefile-write-totalMegabytes" SUS-RunIISummer16MiniAODv2-00001_rt.xml | sed "s/.* Value=\"\(.*\)\".*/\1/")
-        if [ $events -gt 0 ]; then
-            echo "McM Size/event: $(bc -l <<< "scale=4; $size*1024 / $events")"
-        fi
-    fi
-fi
-grep "EventThroughput" SUS-RunIISummer16MiniAODv2-00001_rt.xml 
-if [ $? -eq 0 ]; then
-  var1=$(grep "EventThroughput" SUS-RunIISummer16MiniAODv2-00001_rt.xml | sed "s/.* Value=\"\(.*\)\".*/\1/")
-  echo "McM time_event value: $(bc -l <<< "scale=4; 1/$var1")"
-fi
-echo CPU efficiency info:
-grep "TotalJobCPU" SUS-RunIISummer16MiniAODv2-00001_rt.xml 
-grep "TotalJobTime" SUS-RunIISummer16MiniAODv2-00001_rt.xml 
+
+cmsDriver.py step1 --filein "root://sbgse1.in2p3.fr://dpm/in2p3.fr/home/cms/phedex/store/user/cgrimaul/private_dy_mc/aod_80x/191007_095808/0000/SUS-RunIISummer16DR80Premix-00036_step1_1.root" --fileout file:SMP-RunIISummer16MiniAODv3-00001.root --mc --eventcontent MINIAODSIM --runUnscheduled --datatier MINIAODSIM --conditions 94X_mcRun2_asymptotic_v3 --step PAT --nThreads 1 --era Run2_2016,run2_miniAOD_80XLegacy --python_filename SMP-RunIISummer16MiniAODv3-00001_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n -1 || exit $? ; 
+
+
+#cmsRun -e -j SMP-RunIISummer16MiniAODv3-00001_rt.xml SMP-RunIISummer16MiniAODv3-00001_1_cfg.py || exit $? ; 
+#cmsRun -e -j FrameworkJobReport.xml -p SMP-RunIISummer16MiniAODv3-00001_1_cfg.py || exit $? ; 
+
+#echo 10 events were ran 
+#grep "TotalEvents" SUS-RunIISummer16MiniAODv2-00001_rt.xml 
+#if [ $? -eq 0 ]; then
+#    grep "Timing-tstoragefile-write-totalMegabytes" SUS-RunIISummer16MiniAODv2-00001_rt.xml 
+#    if [ $? -eq 0 ]; then
+#        events=$(grep "TotalEvents" SUS-RunIISummer16MiniAODv2-00001_rt.xml | tail -1 | sed "s/.*>\(.*\)<.*/\1/")
+#        size=$(grep "Timing-tstoragefile-write-totalMegabytes" SUS-RunIISummer16MiniAODv2-00001_rt.xml | sed "s/.* Value=\"\(.*\)\".*/\1/")
+#        if [ $events -gt 0 ]; then
+#            echo "McM Size/event: $(bc -l <<< "scale=4; $size*1024 / $events")"
+#        fi
+#    fi
+#fi
+#grep "EventThroughput" SUS-RunIISummer16MiniAODv2-00001_rt.xml 
+#if [ $? -eq 0 ]; then
+#  var1=$(grep "EventThroughput" SUS-RunIISummer16MiniAODv2-00001_rt.xml | sed "s/.* Value=\"\(.*\)\".*/\1/")
+#  echo "McM time_event value: $(bc -l <<< "scale=4; 1/$var1")"
+#fi
+#echo CPU efficiency info:
+#grep "TotalJobCPU" SUS-RunIISummer16MiniAODv2-00001_rt.xml 
+#grep "TotalJobTime" SUS-RunIISummer16MiniAODv2-00001_rt.xml 
 
